f3154620cb6f20385ed9fc255b240095f67592c0
Musharraf2000 <musharraf.azhar19@gmail.com>
2026-03-12 09:22:03 +0530

    Added UserInputs

--- Week1COM25A1/Program.cs
+++ Week1COM25A1/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Musharraf");
+
+//string name = "Musharraf";
+//name = "Musharraf Azhar";
+//int age = 25;
+//bool isAlive = true;
+//double valueOfPi = 3.14159;
+
+//Console.WriteLine($"My name is {name}");
+
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Your name is {name}");
+
+Console.WriteLine("Enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Your age is {age}");
+
+
